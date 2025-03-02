ee61ca2a4ab3dc1bc58e50576493b88aee747e39
monkspams <synchronize.ultra@gmail.com>
2025-03-02 19:07:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="f1_cli",
-    version="0.1.0",
+    version="1.0.4",
     packages=find_packages(),
     install_requires=[
         "typer[all]",
